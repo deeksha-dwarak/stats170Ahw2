c1dead16a0500b1852f8ef4e9c73003367bf2766
Deeksha Dwarakanath <dwarakad@uci.edu>
2026-02-02 11:33:51 -0800

    Added cs rankings + code to main file

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import csv
+import pandas as pd
 
+def main():
+    df = pd.read_csv("cs_rankings_cleanNames")
+    try:
+        df['clean_name'] = df['clean_name'].astype(str).str.lower()
+        pattern = r"[.]"
+        df['clean_name'] = df['clean_name'].str.replace(pattern, '', regex=True)
+        print("clean names standardized")
+    except:
+        print("something went wrong")
